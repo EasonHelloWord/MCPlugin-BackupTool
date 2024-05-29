1a399f2816efcaa468e7efe77aae2c45743bbb84
Eason_Jan <a13564674280@163.com>
2024-05-29 22:48:45 +0800

    【更新】支持help、tab等功能，完善汉化

--- src/main/java/top/easonjan/backuptool/BackupTool.java
+++ src/main/java/top/easonjan/backuptool/BackupTool.java
@@ -4,6 +4,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -12,9 +13,12 @@ import org.bukkit.scheduler.BukkitTask;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.logging.Level;
 
-public final class BackupTool extends JavaPlugin {
+public final class BackupTool extends JavaPlugin implements TabCompleter {
 
     private FileConfiguration messagesConfig;
     private File messagesFile;
@@ -26,6 +30,8 @@ public final class BackupTool extends JavaPlugin {
         createMessagesFile();
         messagesConfig = YamlConfiguration.loadConfiguration(messagesFile);
         scheduleBackupTask();
+        getCommand("backuptool").setTabCompleter(this);
+        getCommand("bt").setTabCompleter(this);
         getLogger().info("BackupTool enabled!");
     }
 
@@ -43,19 +49,41 @@ public final class BackupTool extends JavaPlugin {
             if (sender.hasPermission("backuptool.backup")) {
                 executeBackup(sender);
             } else {
-                sender.sendMessage(ChatColor.RED + "You do not have permission to use this command.");
+                sendMessage(sender, "no_permission");
             }
             return true;
         } else if (command.getName().equalsIgnoreCase("backuptool") || command.getName().equalsIgnoreCase("bt")) {
-            if (args.length > 0 && args[0].equalsIgnoreCase("reload")) {
-                if (sender.hasPermission("backuptool.admin")) {
-                    reloadConfigurations();
-                    sender.sendMessage(getMessage("reload_complete"));
-                } else {
-                    sender.sendMessage(ChatColor.RED + "You do not have permission to use this command.");
+            if (args.length > 0) {
+                if (args[0].equalsIgnoreCase("backup")) {
+                    if (sender.hasPermission("backuptool.backup")) {
+                        executeBackup(sender);
+                    } else {
+                        sendMessage(sender, "no_permission");
+                    }
+                    return true;
+                } else if (args[0].equalsIgnoreCase("reload")) {
+                    if (sender.hasPermission("backuptool.admin")) {
+                        reloadConfigurations();
+                        sendMessage(sender, "reload_complete");
+                    } else {
+                        sendMessage(sender, "no_permission");
+                    }
+                    return true;
+                } else if (args[0].equalsIgnoreCase("version")) {
+                    sendMessage(sender, "plugin_version", getDescription().getVersion());
+                    return true;
+                } else if (args[0].equalsIgnoreCase("help")) {
+                    sender.sendMessage(ChatColor.GREEN + getMessage("help_command_title"));
+                    sender.sendMessage(ChatColor.GOLD + "/backup" + ChatColor.WHITE + " - " + getMessage("backup_command_description"));
+                    sender.sendMessage(ChatColor.GOLD + "/backuptool backup" + ChatColor.WHITE + " - " + getMessage("backup_command_description"));
+                    sender.sendMessage(ChatColor.GOLD + "/backuptool reload" + ChatColor.WHITE + " - " + getMessage("reload_command_description"));
+                    sender.sendMessage(ChatColor.GOLD + "/backuptool version" + ChatColor.WHITE + " - " + getMessage("version_command_description"));
+                    sender.sendMessage(ChatColor.GOLD + "/backuptool help" + ChatColor.WHITE + " - " + getMessage("help_command_description"));
+                    return true;
                 }
-                return true;
             }
+            sendMessage(sender, "usage_command");
+            return true;
         }
         return false;
     }
@@ -74,7 +102,7 @@ public final class BackupTool extends JavaPlugin {
 
     private void executeBackup(CommandSender sender) {
         String scriptPath = getConfig().getString("backup.script");
-        sender.sendMessage(getMessage("backup_start"));
+        sendMessage(sender, "backup_start");
         new BukkitRunnable() {
             @Override
             public void run() {
@@ -82,13 +110,13 @@ public final class BackupTool extends JavaPlugin {
                     Process process = Runtime.getRuntime().exec(scriptPath);
                     int exitCode = process.waitFor();
                     if (exitCode == 0) {
-                        sender.sendMessage(getMessage("backup_complete"));
+                        sendMessage(sender, "backup_complete");
                     } else {
-                        sender.sendMessage(getMessage("backup_error"));
+                        sendMessage(sender, "backup_error");
                     }
                 } catch (IOException | InterruptedException e) {
                     getLogger().log(Level.SEVERE, "Backup script execution failed", e);
-                    sender.sendMessage(getMessage("backup_error"));
+                    sendMessage(sender, "backup_error");
                 }
             }
         }.runTaskAsynchronously(this);
@@ -113,5 +141,25 @@ public final class BackupTool extends JavaPlugin {
             backupTask.cancel();
         }
         scheduleBackupTask();
+    }
+
+    private void sendMessage(CommandSender sender, String key, String... args) {
+        String message = getMessage(key);
+        if (args.length > 0) {
+            for (int i = 0; i < args.length; i++) {
+                message = message.replace("{" + i + "}", args[i]);
+            }
+        }
+        sender.sendMessage(message);
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        if (command.getName().equalsIgnoreCase("backuptool") || command.getName().equalsIgnoreCase("bt")) {
+            if (args.length == 1) {
+                return Arrays.asList("backup", "reload", "version", "help");
+            }
+        }
+        return new ArrayList<>();
     }
 }
